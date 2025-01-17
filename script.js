d09793505890a66bcb82182475914a3849cccb2f
Baito838 <bagas.aji838@gmail.com>
2025-01-17 20:07:44 +0700

    Update

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function toggleChat() {
         chat.style.display = 'none';
     }
 }
+
+const text = "Hadir untuk mendukung mitra dalam mengembangkan bisnis travel umrah. Dengan solusi yang kami tawarkan, Anda dapat mengelola bisnis travel umrah dengan lebih mudah, transparan, dan effisien."; // Teks yang akan diketik
+const speed = 10; // Kecepatan mengetik dalam milidetik
+typeWriter("hero-description", text, speed);
+
+function typeWriter(elementId, text, speed) {
+    let i = 0; // Index untuk karakter
+    function write() {
+        if (i < text.length) {
+            document.getElementById(elementId).innerHTML += text.charAt(i);
+            i++;
+            setTimeout(write, speed); // Memanggil kembali fungsi setelah jeda
+        }
+    }
+    write(); // Memulai penulisan
+}
